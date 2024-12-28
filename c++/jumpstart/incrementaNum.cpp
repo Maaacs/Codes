62c6e17935c5b9723e24e6c59fe52424fea7818b
Maaacs <msb@icomp.ufam.edu.br>
2024-12-28 01:31:31 -0400

    incrementa

---
+++ c++/jumpstart/incrementaNum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main (){
+
+    int numero;
+
+    std::cout << "Digite um numero\n";
+
+    std::cin >> numero;
+
+    numero++;
+
+    std::cout << numero << "\n";
+
+}
+
+/*
+int main (){
+
+    using namespace std;
+
+    int numero;
+
+    cout << "Digite um numero\n";
+
+    cin >> numero;
+
+    numero++;
+
+    cout << numero << "\n";
+
+}*/
