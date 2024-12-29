2f554b5ea4714f299ebf858f04bdeef92369c455
Maaacs <msb@icomp.ufam.edu.br>
2024-12-29 12:26:05 -0400

    puts

--- c++/jumpstart/insiratexto.cpp
+++ c++/jumpstart/insiratexto.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <cstdio>
+
+void textocomvetor(){
+    char str[100];
+
+    std::cout << "Insira o primeiro texto (Max 100 caracteres)" << std::endl;
+
+     //  std::cin apenas recebe caracteres até o primeiro espaço em branco (null terminator '\0')
+    std::cin.getline(str,100); // std é o namespace que contém o cin que é o objeto. O getline é o método. Com isso conseguimos digitar palavras com espaços '\0' e armazená-las na string. 
+
+    std::cout << "1 - " << str << std::endl;
+}
+
+
+void textocomputs(){
+    char str[50];
+
+    std::cout << "\nInsira o segundo texto (Max 50 caracteres)" << std::endl;
+
+     //  std::cin apenas recebe caracteres até o primeiro espaço em branco (null terminator '\0')
+    std::cin.getline(str,50); // std é o namespace que contém o cin que é o objeto. O getline é o método. Com isso conseguimos digitar palavras com espaços '\0' e armazená-las na string. 
+
+    //std::cout << "2 - " << str << std::endl;
+    puts("2 - "); //  imprime uma cadeia de caracteres no console, e então pula automaticamente uma linha.
+    puts(str); 
+}
+
 
 int main (){
 
-    char str[100];
-
-    std::cout << "Insira o texto (Max 100 caracteres)" << std::endl;
-
-    //  std::cin apenas recebe caracteres até o primeiro espaço em branco (null terminator '\0')
-    std::cin.getline(str,100); // std é o namespace que contém o cin que é o objeto. O getline é o método. Com isso conseguimos digitar palavras com espaços '\0' e armazená-las na string. 
-
-    std::cout << "O texto digitada eh: " << str << std::endl;
-
+    textocomvetor();
+    textocomputs();
+   
     return 0;
-
 }
